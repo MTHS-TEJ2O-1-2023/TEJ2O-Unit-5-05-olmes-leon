e054390acc4b05f52c043014b0ae30e38852c886
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-10-13 13:29:04 -0400

    stuff

--- main.ts
+++ main.ts
@@ -2,7 +2,37 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program uses the RobotBit's Neopixels
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  basic.showIcon(IconNames.Square)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    myStrip.show()
+    basic.pause(100)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow)) 
+    myStrip.show()
+    basic.pause(100)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red)) 
+    myStrip.show()
+    basic.pause(100)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    myStrip.show()
+})
+let myStrip = neopixelStrip
+myStrip.clear()
